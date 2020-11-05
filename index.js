acec1953956b3536aa9061cc4001f865b726bebd
Marialaina <pmarialaina@gmail.com>
2020-11-04 18:17:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+let / const
+let allows reassignment
+const is 'constant' never changing
+deafult to const 
+*/
+
+const age  = 15;
+const name = "Casey";
+
+// if expression is true then {}
+if (age > 10) {
+    console.log('you can get on the ride');
+} else {
+    console.log('too young... sorry');
+}
