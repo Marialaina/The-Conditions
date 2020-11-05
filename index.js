791e9a16b8fd9711e3d2a48ef0c0d9d76239cd91
Marialaina <pmarialaina@gmail.com>
2020-11-04 18:28:40 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ deafult to const
 */
 
 const age  = 15;
-const name = "Casey";
+const name = "Adrian";
 
 // if expression is true then {}
-if (age > 10) {
-    console.log('you can get on the ride');
+if (age > 10 && age < 50){
+    console.log('you can get on the ride')
+} else if (age > 10 && !(age > 10)) {
+    console.log(`you're too old... sorry`);
+} else if (age < 50 && !(age >10)) {
+    console.log(`you're too young, you can't get on... sorry`);
 } else {
-    console.log('too young... sorry');
+    console.log(`sorry you can't get on!`);
 }
+
+if (name === 'Adrian') {
+    console.log('Hey, Adrian');
+} else {
+    console.log(`Hi what's you're name?`);
+}
+
+//ternary
+
+const allowedIn = age > 21 ? true : false;
+console.log(allowedIn);
